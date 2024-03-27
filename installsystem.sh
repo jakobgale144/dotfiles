f9ec7afaef38800cedb5225ddc787415ed606319
jakobgale144 <73265061+jakobgale144@users.noreply.github.com>
2024-03-27 13:13:52 -0400

    Add files via upload

--- installsystem.sh
+++ installsystem.sh
@@ -1,5 +1,4 @@
-#! /bin/sh
-sudo -i
+#!/usr/bin/env bash
 parted /dev/sda -- mklabel gpt
 parted /dev/sda -- mkpart root btrfs 512MB -8GB
 parted /dev/sda -- mkpart swap linux-swap -8GB 100%
@@ -9,26 +8,24 @@ mkfs.fat -F 32 -n boot /dev/sda3
 mkswap -L swap /dev/sda2
 swapon /dev/sda2
 cryptsetup --verify-passphrase -v luksFormat /dev/sda1
-YES
-atgalenegwearesecure
 cryptsetup open /dev/sda1 crypt
-atgalenegwearesecure
-mkfs.btrfs -L root /dev/mapper/crypt
+mkfs.btrfs -L root 
 mount -t btrfs /dev/mapper/crypt /mnt
 btrfs subvolume create /mnt/root
 btrfs subvolume create /mnt/home
 btrfs subvolume create /mnt/nix
 btrfs subvolume create /mnt/persist
+btrfs subvolume create /mnt/var/log
 btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
 umount /mnt
-mount -o subvol=root,compress=zstd,autodefrag,discard,noatime /dev/mapper/crypt /mnt 
+mount -o subvol=root,compress=zstd,discard,noatime /dev/mapper/crypt /mnt 
 mkdir /mnt/home
-mount -o subvol=home,compress=zstd,autodefrag,discard,noatime /dev/mapper/crypt /mnt/home
+mount -o subvol=home,compress=zstd,discard,noatime /dev/mapper/crypt /mnt/home
 mkdir /mnt/nix
-mount -o subvol=nix,compress=zstd,autodefrag,discard,noatime /dev/mapper/crypt /mnt/nix
+mount -o subvol=nix,compress=zstd,discard,noatime /dev/mapper/crypt /mnt/nix
 mkdir /mnt/persist
-mount -o subvol=persist,compress=zstd,autodefrag,discard,noatime /dev/mapper/crypt /mnt/persist
+mount -o subvol=persist,compress=zstd,discard,noatime /dev/mapper/crypt /mnt/persist
 mkdir -p /mnt/var/log
-mount -o subvol=log,compress=zstd,autodefrag,discard,noatime /dev/mapper/crypt /mnt/var/log
+mount -o subvol=log,compress=zstd,discard,noatime /dev/mapper/crypt /mnt/var/log
 mkdir /mnt/boot
 mount -L boot /dev/sda3 /mnt/boot
