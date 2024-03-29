afde3d29b30a50855ffedc76a6a2d0ef53b8ec08
jakobgale144 <73265061+jakobgale144@users.noreply.github.com>
2024-03-29 12:18:51 -0400

    Add files via upload

--- installsystem.sh
+++ installsystem.sh
@@ -1,38 +1,69 @@
 #!/usr/bin/env bash
-parted /dev/sda -- mklabel gpt
-parted /dev/sda -- mkpart root btrfs 512MB -8GB
-parted /dev/sda -- mkpart swap linux-swap -8GB 100%
-parted /dev/sda -- mkpart boot fat32 1MB 512MB
-parted /dev/sda -- set 3 esp on
-mkfs.fat -F 32 -n boot /dev/sda3
-mkswap -L swap /dev/sda2
-swapon /dev/sda2
+
+echo Elevating privileges...
+sudo -i
+
+echo Making GPT labelspace on /dev/sda...
+parted /dev/sda -- mklabel gpt > /dev/null
+echo Making /dev/sda1 (root: btrfs)... 
+parted /dev/sda -- mkpart root btrfs 512MB -8GB > /dev/null
+echo Making /dev/sda2 (swap: linux-swap)...
+parted /dev/sda -- mkpart swap linux-swap -8GB 100% > /dev/null
+echo Making /dev/sda3 (boot: fat32)...
+parted /dev/sda -- mkpart boot fat32 1MB 512MB > /dev/null
+echo Setting /dev/sda3 to EFI System Partition...
+parted /dev/sda -- set 3 esp on > /dev/null
+
+echo Making Fat32 filesystem on /dev/sda3...
+mkfs.fat -F 32 -n boot /dev/sda3 > /dev/null
+echo Making SWAP filesystem on /dev/sda2...
+mkswap -L swap /dev/sda2 > /dev/null
+echo Turning swap on...
+swapon /dev/sda2 > /dev/null
+
+echo Formatting /dev/sda1 for LUKS encryption...
 cryptsetup --verify-passphrase -v luksFormat /dev/sda1
+echo Opening /dev/sda1 into /dev/mapper/crypt...
 cryptsetup open /dev/sda1 crypt
-mkfs.btrfs -L root /dev/mapper/crypt
+echo Making BTRFS filesystem on /dev/mapper/crypt... 
+mkfs.btrfs -L root /dev/mapper/crypt > /dev/null
+echo Mounting /dev/mapper/crypt...
 mount -t btrfs /dev/mapper/crypt /mnt
+echo Creating BTRFS subvolumes...
 btrfs subvolume create /mnt/root
 btrfs subvolume create /mnt/home
 btrfs subvolume create /mnt/nix
 btrfs subvolume create /mnt/persist
 btrfs subvolume create /mnt/log
-btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
+echo Creating readonly snapshot of empty root (erase your darlings)...
+btrfs subvolume snapshot -r /mnt/root /mnt/root-blank > /dev/null
+echo Unmounting base subvolume...
 umount /mnt
-echo "Mounting root..."
+
+echo Mounting root subvolume...
 mount -o subvol=root,compress=zstd,discard,noatime /dev/mapper/crypt /mnt 
 mkdir /mnt/home
-echo "Mounting home..."
+echo Mounting home subvolume...
 mount -o subvol=home,compress=zstd,discard,noatime /dev/mapper/crypt /mnt/home
 mkdir /mnt/nix
-echo "Mounting nix..."
+echo Mounting nix subvolume...
 mount -o subvol=nix,compress=zstd,discard,noatime /dev/mapper/crypt /mnt/nix
 mkdir /mnt/persist
-echo "Mounting persist..."
+echo Mounting persist subvolume...
 mount -o subvol=persist,compress=zstd,discard,noatime /dev/mapper/crypt /mnt/persist
 mkdir -p /mnt/var/log
-echo "Mounting log..."
+echo Mounting log subvolume...
 mount -o subvol=log,compress=zstd,discard,noatime /dev/mapper/crypt /mnt/var/log
 mkdir /mnt/boot
-echo "Mounting boot..."
+
+echo Mounting boot...
 mount /dev/sda3 /mnt/boot
+
+echo Generating NixOS configuration files...
 nixos-generate-config --root /mnt
+echo Copying original hardware config...
+mv /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/BACKUP-hardware-configuration.nix
+echo Moving custom hardware configuration into place...
+cp ~/dotfiles/hardware-configuration.nix /mnt/etc/nixos
+
+echo Edit your configuration files now, and the nixos-install when you are ready.
