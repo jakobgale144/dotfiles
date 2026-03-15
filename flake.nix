8bdb662427a1e2362889f562c1a8bee29c58f661
Jakob Gale <jakobgale144@gmail.com>
2026-03-15 14:46:21 -0400

    Fix

--- flake.nix
+++ flake.nix
@@ -6,17 +6,17 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     # Nixpkgs Unstable
-    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Preservation
     preservation.url = "github:nix-community/preservation";
   };
 
-  outputs = inputs @ {
+  outputs = {
     self,
     nixpkgs,
-    # nixpkgs-unstable,
-  }: {
+    ...
+  } @ inputs: {
     nixosConfigurations = import ./hosts inputs;
   };
 }
