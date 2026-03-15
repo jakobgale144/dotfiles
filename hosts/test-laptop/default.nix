ccf1adc3573106b086cd8364c7850a3b2951a3ba
Jakob Gale <jakobgale144@gmail.com>
2026-03-14 21:57:29 -0400

    Bugfix

--- hosts/test-laptop/default.nix
+++ hosts/test-laptop/default.nix
@@ -1,28 +1,22 @@
 { ... }:
-{
-  # imports = [ 
-  #   (modulesPath + "/installer/scan/not-detected.nix")
-  # ];
+let
+  hostvars = import ./vars.nix;
+in {
+  boot.initrd.availableKernelModules = [ # todo: add more based on errors 
+    "nvme"
+    "ahci"
+    "usbhid"
+    "xhci_pci"
+    "usb_storage"
+    "sd_mod"
+    "sdhci_pci"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ]; # KVM support
+  boot.extraModulePackages = [ ];
 
-  let
-    hostvars = import ./vars.nix;
-  in {
-    boot.initrd.availableKernelModules = [ # todo: add more based on errors 
-      "nvme"
-      "ahci"
-      "usbhid"
-      "xhci_pci"
-      "usb_storage"
-      "sd_mod"
-      "sdhci_pci"
-    ];
-    boot.initrd.kernelModules = [ ];
-    boot.kernelModules = [ "kvm-amd" ]; # KVM support
-    boot.extraModulePackages = [ ];
+  boot.initrd.luks.devices."crypt".device = "/dev/disk/by-uuid/${hostvars.test-laptop.primary_uuid}";
 
-    boot.initrd.luks.devices."crypt".device = "/dev/disk/by-uuid/${hostvars.test-laptop.primary_uuid}";
-
-    # Our boot device
-    fileSystems."/boot".device = "/dev/disk/by-uuid/${hostvars.test-laptop.boot_uuid}";
-  };
-}
+  # Our boot device
+  fileSystems."/boot".device = "/dev/disk/by-uuid/${hostvars.test-laptop.boot_uuid}";
+};
