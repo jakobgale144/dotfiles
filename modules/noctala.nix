16a8fd0803832e65bc76adde4cfe5f18defade22
Jakob Gale <jakobgale144@gmail.com>
2026-03-28 17:37:34 -0400

    Delete noctalia

--- modules/noctala.nix
+++
@@ -1,8 +0,0 @@
-{ self, inputs, ... }:
-{
-  perSystem = { pkgs, ... }: {
-    packages.noctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
-      settings = (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings;
-    };
-  };
-}
