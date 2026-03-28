f7e43482ef87148575eb10ef63ef46a14fe0bbc4
Jakob Gale <jakobgale144@gmail.com>
2026-03-28 19:45:56 -0400

    Bugfix

--- modules/user.nix
+++ modules/user.nix
@@ -1,19 +1,19 @@
 { den, ... }:
 {
-  den.aspects.test = { user, ... }: {
+  den.aspects.test = {
     includes = [
       den.aspects.desktop
       den.aspects.packages
     ];
 
     hjem = {
-     user = user.userName;
-     directory = "/home/${user.userName}"; 
+     user = "test";
+     directory = "/home/test"; 
     };
 
     nixos = {
       users.users.root.initialPassword = "jkl";
-      users.users.${user.userName} = {
+      users.users.test = {
         initialPassword = "jkl";
         isNormalUser = true;
         extraGroups = [ "wheel" ];
