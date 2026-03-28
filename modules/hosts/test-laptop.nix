4aabaa17e10309d5843dcc47ed228dd44623f927
Jakob Gale <jakobgale144@gmail.com>
2026-03-28 18:14:02 -0400

    Bugfix

--- modules/hosts/test-laptop.nix
+++ modules/hosts/test-laptop.nix
@@ -2,19 +2,21 @@
 {
   den.hosts.x86_64-linux.test-laptop.users.test = { };
 
-  den.aspects.test-laptop.includes = [
-    den.aspects.boot
-    den.aspects.filesystems
-    den.aspects.preservation
-    den.aspects.system
-
-    den.aspects.test-laptop-hardware
-  ];
-
-  den.aspects.test-laptop-hardware = {
+  den.aspects.test-laptop = {
     primaryUuid = "c5903369-fc45-44f8-b248-ef7260f24e92";
     bootUuid = "5928-46CE";
 
+    includes = [
+      den.aspects.boot
+      den.aspects.filesystems
+      den.aspects.preservation
+      den.aspects.system
+
+      den.aspects.test-laptop-hardware
+    ];
+  };
+
+  den.aspects.test-laptop-hardware = {
     nixos = { lib, config, ... }: {
       boot.initrd.availableKernelModules = [ # todo: add more based on errors 
         "nvme"
