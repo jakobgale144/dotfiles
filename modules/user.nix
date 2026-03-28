6d8f26795101487cc60a64791bfe159afed3a251
Jakob Gale <jakobgale144@gmail.com>
2026-03-28 19:40:29 -0400

    Bugfix

---
+++ modules/user.nix
@@ -0,0 +1,22 @@
+{
+  den.aspects.test = { user, ... }: {
+    includes = [
+      den.aspects.desktop
+      den.aspects.packages
+    ];
+
+    hjem = {
+     user = user.userName;
+     directory = "/home/${user.userName}"; 
+    };
+
+    nixos = {
+      users.users.root.initialPassword = "jkl";
+      users.users.${vars.username} = {
+        initialPassword = "jkl";
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+      };
+    };
+  };
+}
