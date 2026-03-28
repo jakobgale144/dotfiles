dc326c7b9dae842f1e4c08731bf56d628fe77078
Jakob Gale <jakobgale144@gmail.com>
2026-03-28 18:01:43 -0400

    Bugfix

--- modules/system.nix
+++ modules/system.nix
@@ -3,22 +3,22 @@
     nixpkgs.config.allowUnfree = true; # Allows proprietary packages
 
     nix = {
-    # inputs, lib, pkgs, ... }:
-    # let
-    #   flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-    # in {
-    #   settings = {
-    #     experimental-features = "nix-command flakes"; # Enable Flakes and the new "nix" command
-    #     flake-registry = ""; # Disable global registry
-    #   };
-    #   channel.enable = false; # Disable channels
+    inputs, lib, pkgs, ... }:
+    let
+      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+    in {
+      settings = {
+        experimental-features = "nix-command flakes"; # Enable Flakes and the new "nix" command
+        flake-registry = ""; # Disable global registry
+      };
+      channel.enable = false; # Disable channels
 
-    #   # Make flake registry and Nix path match flake inputs
-    #   registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    #   nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-    # };
+      # Make flake registry and Nix path match flake inputs
+      registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
 
-    networking.networkmanager.enable = true;
-    time.timeZone = "America/New_York";
+      networking.networkmanager.enable = true;
+      time.timeZone = "America/New_York";
+    };
   };
 }
