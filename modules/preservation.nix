8929a447a64c3d88314fa10885d09036cde11112
Jakob Gale <jakobgale144@gmail.com>
2026-03-28 19:12:28 -0400

    Bugfix

--- modules/preservation.nix
+++ modules/preservation.nix
@@ -1,140 +1,144 @@
 { inputs, den, ... }:
 {
-  den.aspects.preservation = den.lib.perUser { lib, pkgs, ... }: {
+  den.aspects.preservation = den.lib.parametric {
     imports = [ inputs.preservation.nixosModules.default ];
 
-    preservation.enable = true;
-    preservation.preserveAt."/persist" = {
-      directories = [
-        "/etc/NetworkManager/system-connections"
-        "/etc/ssh"
-        "/etc/nix/inputs"
-        # "/etc/agenix"
+    includes = [
+      ({user, lib, pkgs, ...}: {
+        preservation.enable = true;
+        preservation.preserveAt."/persist" = {
+          directories = [
+            "/etc/NetworkManager/system-connections"
+            "/etc/ssh"
+            "/etc/nix/inputs"
+            # "/etc/agenix"
 
-        "/var/log"
+            "/var/log"
 
-        "/var/lib/nixos"
-        "/var/lib/systemd"
-        {
-          directory = "/var/lib/private";
-          mode = "0700";
-        }
+            "/var/lib/nixos"
+            "/var/lib/systemd"
+            {
+              directory = "/var/lib/private";
+              mode = "0700";
+            }
 
-        # todo: virtualization
+            # todo: virtualization
 
-        # "/var/lib/bluetooth"
-        "/var/lib/NetworkManager"
-      ];
+            # "/var/lib/bluetooth"
+            "/var/lib/NetworkManager"
+          ];
     
-      files = [
-        {
-          file = "/etc/machine-id";
-          inInitrd = true;
-        }
-        # {
-        #   file = "/etc/shadow";
-        #   mode = "0700";
-        # }
-      ];
+          files = [
+            {
+              file = "/etc/machine-id";
+              inInitrd = true;
+            }
+            # {
+            #   file = "/etc/shadow";
+            #   mode = "0700";
+            # }
+          ];
 
-      users.${user.userName} = {
-        commonMountOptions = [
-          "x-gvfs-hide"
-        ];
-        directories = [
-          # XDG Home Directories
-          "Desktop"
-          "Documents"
-          "Downloads"
-          "Pictures"
-          "Videos"
+          users.${user.userName} = {
+            commonMountOptions = [
+              "x-gvfs-hide"
+            ];
+            directories = [
+              # XDG Home Directories
+              "Desktop"
+              "Documents"
+              "Downloads"
+              "Pictures"
+              "Videos"
 
-          ".cache"
+              ".cache"
 
-          # Personal Directories
-          "Repos"
-          "Projects"
-          "NixOS"
-          "Temp"
+              # Personal Directories
+              "Repos"
+              "Projects"
+              "NixOS"
+              "Temp"
 
-          # Nix / Home Manager Profiles
-          ".local/state/home-manager"
-          ".local/state/nix/profiles"
-          ".local/share/nix"
+              # Nix / Home Manager Profiles
+              ".local/state/home-manager"
+              ".local/state/nix/profiles"
+              ".local/share/nix"
 
-          # todo: editors
+              # todo: editors
 
-          # Language Package Managers
-          ".cargo"
-          # ".local/share/uv"
+              # Language Package Managers
+              ".cargo"
+              # ".local/share/uv"
 
-          # Security
-          {
-            directory = ".gnupg";
-            mode = "0700";
-          }
-          {
-            directory = ".ssh";
-            mode = "0700";
-          }
-          {
-            directory = ".pki"; # todo: look into this...
-            mode = "0700";
-          }
-          # {
-          #   directory = ".local/share/password-store";
-          #   mode = "0700";
-          # }
+              # Security
+              {
+                directory = ".gnupg";
+                mode = "0700";
+              }
+              {
+                directory = ".ssh";
+                mode = "0700";
+              }
+              {
+                directory = ".pki"; # todo: look into this...
+                mode = "0700";
+              }
+              # {
+              #   directory = ".local/share/password-store";
+              #   mode = "0700";
+              # }
 
-          # Games
-          ".steam"
-          # ".config/lutris" # Can this config go anywhere else?
-          ".local/share/Steam"
-          ".local/share/lutris"
+              # Games
+              ".steam"
+              # ".config/lutris" # Can this config go anywhere else?
+              ".local/share/Steam"
+              ".local/share/lutris"
 
-          # Browsers
-          ".mozilla"
+              # Browsers
+              ".mozilla"
 
-          # CLI Data
-          ".local/share/atuin"
-          ".local/share/zoxide"
-        ];
-      };
-    };
-
-    nixos = {
-      boot.initrd.systemd.enable = true;
-      systemd.tmpfiles.settings.preservation =
-        let
-          permission = {
-            user = user.userName;
-            group = lib.mkForce user.userName;
-            mode = lib.mkForce "0750";
+              # CLI Data
+              ".local/share/atuin"
+              ".local/share/zoxide"
+            ];
           };
-        in {
-          "/home/${user.userName}/.config".d = permission;
-          "/home/${user.userName}/.local".d = permission;
-          "/home/${user.userName}/.local/share".d = permission;
-          "/home/${user.userName}/.local/state".d = permission;
-          "/home/${user.userName}/.local/state/nix".d = permission;
         };
 
-      systemd.suppressedSystemUnits = [ "systemd-machine-id-commit.service" ];
+        nixos = {
+          boot.initrd.systemd.enable = true;
+          systemd.tmpfiles.settings.preservation =
+            let
+              permission = {
+                user = user.userName;
+                group = lib.mkForce user.userName;
+                mode = lib.mkForce "0750";
+              };
+            in {
+              "/home/${user.userName}/.config".d = permission;
+              "/home/${user.userName}/.local".d = permission;
+              "/home/${user.userName}/.local/share".d = permission;
+              "/home/${user.userName}/.local/state".d = permission;
+              "/home/${user.userName}/.local/state/nix".d = permission;
+            };
 
-      systemd.services.systemd-machine-id-commit = {
-        unitConfig.ConditionPathIsMountPoint = [
-          ""
-          "/persist/etc/machine-id"
-        ];
-        serviceConfig.ExecStart = [
-          ""
-          "systemd-machine-id-setup --commit --root /persist"
-        ];
-      };
+          systemd.suppressedSystemUnits = [ "systemd-machine-id-commit.service" ];
 
-      # The below fixes an issue on first bootup where systemd attempts to look for
-      # the defined groups and users before those users/groups have been created.
-      # systemd.services."systemd-tmpfiles-setup".after = [ "systemd-sysusers.service" ];
-    };
+          systemd.services.systemd-machine-id-commit = {
+            unitConfig.ConditionPathIsMountPoint = [
+              ""
+              "/persist/etc/machine-id"
+            ];
+            serviceConfig.ExecStart = [
+              ""
+              "systemd-machine-id-setup --commit --root /persist"
+            ];
+          };
+
+          # The below fixes an issue on first bootup where systemd attempts to look for
+          # the defined groups and users before those users/groups have been created.
+          # systemd.services."systemd-tmpfiles-setup".after = [ "systemd-sysusers.service" ];
+        };
+      })
+    ];
   };
 }
