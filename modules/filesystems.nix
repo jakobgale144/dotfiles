afc41c9ec074278847b2e53a188bf2d989560ea5
Jakob Gale <jakobgale144@gmail.com>
2026-03-28 19:17:27 -0400

    Bugfix

--- modules/filesystems.nix
+++ modules/filesystems.nix
@@ -2,9 +2,9 @@
 {
   den.aspects.filesystems = den.lib.parametric {
     includes = [
-      ({ host, ... }: { # todo: fix so I don't need lib
-          nixos.boot.initrd.luks.devices."crypt".device = "/dev/disk/by-uuid/${host.primaryUuid}";
-          nixos.fileSystems."/boot".device = "/dev/disk/by-uuid/${host.bootUuid}";
+      ({ host }: {
+        nixos.boot.initrd.luks.devices."crypt".device = "/dev/disk/by-uuid/${host.primaryUuid}";
+        nixos.fileSystems."/boot".device = "/dev/disk/by-uuid/${host.bootUuid}";
       })
     ];
 
