22789ae3096f4b9e3cc9c002a9d8f8c759e1f538
Jakob Gale <jakobgale144@gmail.com>
2026-03-28 18:40:07 -0400

    Bugfix

--- modules/filesystems.nix
+++ modules/filesystems.nix
@@ -1,3 +1,4 @@
+{ den, ... }:
 {
   den.aspects.filesystems = den.lib.parametric {
     includes = [
@@ -19,6 +20,8 @@
       fileSystems."/" = {
         device = "tmpfs"; # Erase your darlings!
         fsType = "tmpfs";
+boot.initrd.luks.devices."crypt".device = "/dev/disk/by-uuid/${host.primaryUuid}";
+    device = "/dev/disk/by-uuid/${host.bootUuid}";
         options = [
           "relatime"
           "mode=755" # systemd defaults to 777, which sometimes causes problems
