4f19b0b7a257b34d1b9f6c44bdff159c24889ac2
Jakob Gale <jakobgale144@gmail.com>
2026-03-28 18:08:01 -0400

    Bugfix

--- modules/system.nix
+++ modules/system.nix
@@ -1,22 +1,20 @@
 {
-  den.aspects.system.nixos = {
+  den.aspects.system.nixos = { inputs, lib, pkgs, ... }:
+  let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
     nixpkgs.config.allowUnfree = true; # Allows proprietary packages
     networking.networkmanager.enable = true;
     time.timeZone = "America/New_York";
 
-    ({ inputs, lib, pkgs, ... }:
-    let
-      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-    in {
-      settings = {
-        experimental-features = "nix-command flakes"; # Enable Flakes and the new "nix" command
-        flake-registry = ""; # Disable global registry
-      };
-      channel.enable = false; # Disable channels
+    settings = {
+      experimental-features = "nix-command flakes"; # Enable Flakes and the new "nix" command
+      flake-registry = ""; # Disable global registry
+    };
+    channel.enable = false; # Disable channels
 
-      # Make flake registry and Nix path match flake inputs
-      registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-    };)
+    # Make flake registry and Nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 }
