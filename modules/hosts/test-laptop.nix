6b6e3101351c9bacfa0381f5e924a463c8357ca4
Jakob Gale <jakobgale144@gmail.com>
2026-03-28 19:30:33 -0400

    Bugfix

--- modules/hosts/test-laptop.nix
+++ modules/hosts/test-laptop.nix
@@ -1,11 +1,14 @@
 { den, ... }:
 {
-  den.hosts.x86_64-linux.test-laptop.users.test = { };
+  den.hosts.x86_64-linux.test-laptop = {
+    users.test = { };
 
-  den.aspects.test-laptop = {
     primaryUuid = "c5903369-fc45-44f8-b248-ef7260f24e92";
     bootUuid = "5928-46CE";
+  };
 
+  den.aspects.test-laptop = {
+    # todo: move the below to test-laptop-hardware
     includes = [
       den.aspects.boot
       den.aspects.filesystems
