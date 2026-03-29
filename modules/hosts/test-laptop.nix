976a93d0836c905fe4bbd40bf481b7eba51087e7
Jakob Gale <jakobgale144@gmail.com>
2026-03-29 15:05:05 -0400

    Bugfix

--- modules/hosts/test-laptop.nix
+++ modules/hosts/test-laptop.nix
@@ -1,6 +1,8 @@
 { den, ... }:
 {
-  den.hosts.x86_64-linux.test-laptop.users.test = {
+  den.hosts.x86_64-linux.test-laptop = {
+    users.test = { };
+    
     primaryUuid = "c5903369-fc45-44f8-b248-ef7260f24e92";
     bootUuid = "5928-46CE";
 
