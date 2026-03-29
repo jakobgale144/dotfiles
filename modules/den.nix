737279551ec513ca9aa90be1849d6c5bee9e68c6
Jakob Gale <jakobgale144@gmail.com>
2026-03-29 13:54:13 -0400

    Bugfix

--- modules/den.nix
+++ modules/den.nix
@@ -15,7 +15,7 @@
     options.bootUuid = lib.mkOption;
     options.primaryUuid = lib.mkOption;
     
-    hjem.enable = lib.mkDefault true; # todo: fix? necessary?
-    hjem.clobberByDefault = lib.mkDefault true;
+    hjem.enable = true; # todo: fix? necessary?
+    hjem.clobberByDefault = true;
   };
 }
