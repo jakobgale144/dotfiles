6dba7d7c26e17c2d043639c324e38f61e404c694
Jakob Gale <jakobgale144@gmail.com>
2026-04-01 22:25:33 -0400

    getting nushell to work

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,25 +1,27 @@
 { den, ... }:
 {
-  den.aspects.packages = {
-    includes = [
-      ( den._.user-shell "nushell" )
+  den.aspects.packages = { user, pkgs, ... }: {
+    hjem.packages = [
+      # pkgs.yazelix
+      pkgs.helix
+      pkgs.nushell
+      pkgs.wezterm
+      pkgs.git
+      # pkgs.zen-browser
+      pkgs.firefox
+      pkgs.github-cli
     ];
 
-    hjem = { pkgs, ...}: {
-      packages = [
-        # pkgs.yazelix
-        pkgs.helix
-        pkgs.wezterm
-        pkgs.git
-        # pkgs.zen-browser
-        pkgs.firefox
-        pkgs.github-cli
-      ];
-
-      files = {
-        ".config/helix/config.toml".source = ./config/helix.toml;
-        ".config/helix/languages.toml".source = ./config/helix-languages.toml;
-      };
+    hjem.files = {
+      ".config/helix/config.toml".source = ./config/helix.toml;
+      ".config/helix/languages.toml".source = ./config/helix-languages.toml;
     };
+
+    hjem.environment.sessionVariables = {
+      EDITOR = "hx";
+      VISUAL = "hx";
+    };
+
+    nixos.users.users.${user.userName}.shell = pkgs.nushell;
   };
 }
