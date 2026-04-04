5e0afb58c0262d2e4a74baf6df0a2518d287ef06
Jakob Gale <jakobgale144@gmail.com>
2026-04-04 15:17:28 -0400

    bugfix

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -14,7 +14,7 @@
     };
   };
 
-  den.aspects.noctalia = { inputs, ... }:
+  den.aspects.noctalia = { inputs', ... }:
   let
     noctaliaPkg = inputs'.nixpkgs-unstable.legacyPackages.noctalia-shell;
   in {
